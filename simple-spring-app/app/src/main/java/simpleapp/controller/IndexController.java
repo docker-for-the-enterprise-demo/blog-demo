9269b4bc138e512472c383b7720f8e4a453b05b1
Andrew DeMaria <lostonamountain@gmail.com>
2015-01-17 12:21:48 -0600

    Deleted old pastebin application controllers

--- simple-spring-app/app/src/main/java/simpleapp/controller/IndexController.java
+++
@@ -1,7 +0,0 @@
-package simpleapp.controller;
-
-public class IndexController {
-	public String index() {
-		return "Nice! You've hit the index controller. Now try /pastebin/upload?content=helloworld";
-	}
-}
